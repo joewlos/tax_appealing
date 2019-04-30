34a5e02c68de398094bd98bc7716dfacc3862c29
Joe Wlos <jwlos@DWIGHT.local>
2019-04-30 16:02:49 -0400

    Form updates

--- forms/appeal.py
+++ forms/appeal.py
@@ -6,10 +6,66 @@ APPEAL FORM
 from flask_wtf import FlaskForm
 from wtforms import SelectField, StringField, validators
 
-# Class for filling appeal PDF
+# Class for filling appeal form
 class AppealForm(FlaskForm):
 
-	# Options for box 1 dropdown
+	# States for form
+	states = {
+		'AK': 'Alaska',
+		'AL': 'Alabama',
+		'AR': 'Arkansas',
+		'AZ': 'Arizona',
+		'CA': 'California',
+		'CO': 'Colorado',
+		'CT': 'Connecticut',
+		'DC': 'District of Columbia',
+		'DE': 'Delaware',
+		'FL': 'Florida',
+		'GA': 'Georgia',
+		'HI': 'Hawaii',
+		'IA': 'Iowa',
+		'ID': 'Idaho',
+		'IL': 'Illinois',
+		'IN': 'Indiana',
+		'KS': 'Kansas',
+		'KY': 'Kentucky',
+		'LA': 'Louisiana',
+		'MA': 'Massachusetts',
+		'MD': 'Maryland',
+		'ME': 'Maine',
+		'MI': 'Michigan',
+		'MN': 'Minnesota',
+		'MO': 'Missouri',
+		'MS': 'Mississippi',
+		'MT': 'Montana',
+		'NC': 'North Carolina',
+		'ND': 'North Dakota',
+		'NE': 'Nebraska',
+		'NH': 'New Hampshire',
+		'NJ': 'New Jersey',
+		'NM': 'New Mexico',
+		'NV': 'Nevada',
+		'NY': 'New York',
+		'OH': 'Ohio',
+		'OK': 'Oklahoma',
+		'OR': 'Oregon',
+		'PA': 'Pennsylvania',
+		'PR': 'Puerto Rico',
+		'RI': 'Rhode Island',
+		'SC': 'South Carolina',
+		'SD': 'South Dakota',
+		'TN': 'Tennessee',
+		'TX': 'Texas',
+		'UT': 'Utah',
+		'VA': 'Virginia',
+		'VT': 'Vermont',
+		'WA': 'Washington',
+		'WI': 'Wisconsin',
+		'WV': 'West Virginia',
+		'WY': 'Wyoming'
+	}
+
+	# Options for box 1 dropdowns
 	taxpayer_options = [
 		'Owner',
 		'Former Owner Liable for Tax',
@@ -33,11 +89,36 @@ class AppealForm(FlaskForm):
 	]
 
 	# Box 1 fields
-	name = StringField('Taxpayer_Name', [validators.Length(min=4, max=120)])
-	email = StringField('Taxpayer_Email', [validators.Length(min=4, max=120)])
-	address = StringField('Taxpayer_Street', [validators.Length(min=4, max=120)])
-	city = StringField('Taxpayer_City', [validators.Length(min=4, max=120)])
-	state = StringField('Taxpayer_State', [validators.Length(min=2, max=2)])
+	name = StringField('Taxpayer_Name', 
+		validators=[
+			validators.Length(min=4, max=120),
+			validators.DataRequired(message='Name Required')
+		]
+	)
+	email = StringField('Taxpayer_Email', 
+		validators=[
+			validators.Email(message='Invalid Email Address'),
+			validators.DataRequired(message='Email Required')
+		]
+	)
+	address = StringField('Taxpayer_Street', 
+		validators=[
+			validators.Length(min=4, max=120),
+			validators.DataRequired(message='Street Address Required')
+		]
+	)
+	city = StringField('Taxpayer_City', 
+		validators=[
+			validators.Length(min=4, max=120),
+			validators.DataRequired(message='City Required')
+		]
+	)
+	state = SelectField('Taxpayer_State', 
+		choices=states.keys(),
+		validators=[
+			validators.InputRequired(message='State Required')
+		]
+	)
 	zipcode = StringField('Taxpayer_Zipcode', [validators.Length(min=5, max=5)])
 	phone = StringField('Taxpayer_Phone', [validators.Length(min=4, max=120)])
 	relation = SelectField('Taxpayer_Relation', choices=taxpayer_options, 
